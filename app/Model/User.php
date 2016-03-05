5358e9a30ed860a6fb5c88a0da4372f8973b3c38
adhikasp <adhika.setyap@gmail.com>
2016-03-05 14:20:04 +0700

    Unhidden password field
    I only do this because legacy requirement

--- app/Model/User.php
+++ app/Model/User.php
@@ -18,30 +18,12 @@ class User extends Authenticatable
     protected $fillable = [
         'user_id', 'name', 'email', 'password',
     ];
-<<<<<<< c24cd5e0d12b54586f7384d7fd588bcfe2e8735d
-    
-    /**
-     * one user can have many access to many device, many port
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-=======
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
 
     /**
      * one user can have many access to many device, many port
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
->>>>>>> Move Model class that in root app/ into app/Model/ namespace
     function deviceUser(){
         return $this->hasMany('\App\UserDevice', 'user_id', 'user_id');
     }
