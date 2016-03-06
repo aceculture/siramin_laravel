dc9947a765934463ef19d37c7eba7e6e7dd69914
adhikasp <adhika.setyap@gmail.com>
2016-03-06 11:46:31 +0700

    Add watering set method endpoint

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -22,4 +22,38 @@ class DeviceController extends Controller
         dd($response);
         return response()->json($response);
     }
+
+    public function setWateringMode(Request $request, $device_token)
+    {
+        $param = $request->getContent();
+        $param = json_decode($param, true);
+
+        $port = DevicePort::where('device_token', $device_token)
+            ->where('port_number', $param['id'])
+            ->first();
+
+        $response = [
+            'code' => 200,
+            'data' => []
+        ];
+
+        if (isset($param['status'])) {
+            if (in_array($param['status'], ['semi', 'otomatis'], true)) {
+                $port->watering_method = $param['status'];
+                $port->save();
+
+                $response['data'] = [];
+            }
+            else {
+                // If incoming request have invalid watering method,
+                // return an error msg
+                $response['data'] = ['error' => 'Watering method is not recognized.'];
+            }
+        }
+        else {
+            $response['data'] = ['error' => 'Watering method value is not found.'];
+        }
+
+        return response()->json($response);
+    }
 }
